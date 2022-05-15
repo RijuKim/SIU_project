29d82e8cd948a5fce58083d70ea1fd8314dea8b0
Rijoo <rijoo1121@gmail.com>
2022-05-15 21:59:03 +0900

    Add: 파이어베이스 연결

--- app/src/main/java/com/example/siu_project/TodoWriteActivity.kt
+++ app/src/main/java/com/example/siu_project/TodoWriteActivity.kt
@@ -1,9 +1,19 @@
 package com.example.siu_project
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.core.widget.doAfterTextChanged
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class TodoWriteActivity : AppCompatActivity() {
+    var content: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo_write)
